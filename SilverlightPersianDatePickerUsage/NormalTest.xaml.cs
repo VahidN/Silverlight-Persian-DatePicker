4d524ad96b0f17ed70de2f0113fa366c703275c4
Vahid Nasiri <VahidN@users.noreply.github.com>
2011-06-04 13:38:51 +0000

    Some minor fixes.

---
+++ SilverlightPersianDatePickerUsage/NormalTest.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace SilverlightPersianDatePickerUsage
+{
+    public partial class NormalTest
+    {
+        public NormalTest()
+        {
+            InitializeComponent();
+            this.Loaded += NormalTest_Loaded;
+        }
+
+        void NormalTest_Loaded(object sender, RoutedEventArgs e)
+        {
+            PDatePicker1.SelectedDate = DateTime.Now.ToString();
+            PDatePicker2.SelectedPersianDate = "1390/03/10";
+        }
+    }
+}
