102f7fbebfecfedf7d49e708291455d7665eeeb3
Vahid Nasiri <VahidN@users.noreply.github.com>
2011-12-16 21:18:37 +0000

    Added Silverlight 5 support.

--- SilverlightPersianDatePickerUsage/NormalTest.xaml.cs
+++ SilverlightPersianDatePickerUsage/NormalTest.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace SilverlightPersianDatePickerUsage
 {
@@ -9,6 +11,22 @@ namespace SilverlightPersianDatePickerUsage
         {
             InitializeComponent();
             this.Loaded += NormalTest_Loaded;
+            RegisterForNotification(this.PDatePicker1, "SelectedPersianDate",
+                (d, e) =>
+                {
+                    //Do something ...
+                });
+        }
+
+        public void RegisterForNotification(FrameworkElement element, string propertyName, PropertyChangedCallback callback)
+        {
+            var b = new Binding(propertyName) { Source = element };
+            var prop = DependencyProperty.RegisterAttached(
+                "ListenAttached" + propertyName,
+                typeof(object),
+                typeof(UserControl),
+                new PropertyMetadata(callback));
+            element.SetBinding(prop, b);
         }
 
         void NormalTest_Loaded(object sender, RoutedEventArgs e)
