12cd738eac184879477ade43ca32c93ef03e4607
Vahid Nasiri <VahidN@users.noreply.github.com>
2012-10-09 20:11:07 +0000

    - Added WinForms Sample.

---
+++ WinFormSample/frmMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormSample
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnGetDate_Click(object sender, EventArgs e)
+        {
+            var datePicker = (WpfPersianDatePicker.Views.PDatePicker)elementHost1.Child;            
+            lblDate.Text = datePicker.SelectedPersianDate;
+        }
+    }
+}
